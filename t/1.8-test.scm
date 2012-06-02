824f432af411f5087cde1972af2b3e7e7e121868
Freddie FUJIWARA <github@ze.gs>
2012-06-02 10:07:34 +0000

    dummy fail

--- t/1.8-test.scm
+++ t/1.8-test.scm
@@ -7,5 +7,5 @@
 (test "1  =>  1.0" 1.0 (lambda () (round (cbrt 1))))
 (test "8  =>  2.0" 2.0 (lambda () (round (cbrt 8))))
 (test "27  => 3.0" 3.0 (lambda () (round (cbrt 27))))
-(test "64  => 4.0" 4.0 (lambda () (round (cbrt 27))))
+(test "64  => 4.0" 4.0 (lambda () (round (cbrt 26))))
 (test-end :exit-on-failure #t)
